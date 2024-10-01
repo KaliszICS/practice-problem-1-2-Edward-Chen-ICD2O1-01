06e94325684173651a2e7cf03c59bfc3d467f6e8
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-10-01 14:13:10 +0000

    jh

--- PP1_2.py
+++ PP1_2.py
@@ -3,18 +3,37 @@
 #Write Assignment code here
 
 def q1():
+    _name_="Alex"
+    print(_name_)
 
 def q2():
+    num=3
+    num1=5
+    num2=2
+    print(num+num1+num2)
 
 def q3():
+    num3=21
+    num4=4
+    print(num3//num4)
 
 def q4():
+    num5=6.3
+    num6=3.0
+    print(num5/num6)
+
 
 def q5():
-
+    words="Hello World"
+    words="Goodbye World"
 def q6():
+    _True_= True
+    print(True)
 
 def q7():
+    num7=7
+    num8=(2)
+    print(num7/num8)
 
 q1()
 q2()
